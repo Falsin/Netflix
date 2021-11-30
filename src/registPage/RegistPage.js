9e09609088806cb59d92c1faaf2bcf97030e24ee
Falsin <greenldend@gmail.com>
2021-12-01 06:55:55 +0700

    s

--- src/registPage/RegistPage.js
+++ src/registPage/RegistPage.js
@@ -6,18 +6,71 @@ import {
   signInWithPopup,
   signOut,
   signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Header from '../commonTemplate/Header';
 import app from '../Firebase';
 
 export default function RegistPage(params) {
-  initFirebaseAuth()
+  const [userDate, setUserDate] = useState(null);
+
+  initFirebaseAuth();
+
+  function initFirebaseAuth() {
+    onAuthStateChanged(getAuth(), authStateObserver);
+  }
+
+  function authStateObserver(user) {
+    /* console.log('observe');
+    console.log(user) */
+    if (user) {
+      try {
+        if(userDate === null || (userDate.userName !== getUserName() && userDate.userPicElem !== getProfilePicUrl())) {
+          setUserDate({
+            userName: getUserName(),
+            userPicElem: getProfilePicUrl(),
+          })
+          console.log('work')
+        }
+      } catch(err) {
+        console.log('nothing was happened!')
+      }
+    }
+    /* if (user) {
+      setUserDate({
+        userName: getUserName(),
+        userPicElem: getProfilePicUrl(),
+      })
+    } else {
+      setUserDate(null)
+    } */
+  
+  
+    /* let signInBtn     = document.getElementById('sign-in');
+    let signOutBtn    = document.getElementById('sign-out');
+    let userNameElem  = document.getElementById('user-name');
+    let userPicElem   = document.getElementById('user-pic');
+  
+    if (user) {
+      userNameElem.textContent = getUserName();
+      userPicElem.style.backgroundImage = `url(${getProfilePicUrl()})`
+  
+      signInBtn.style.display = 'none';
+      signOutBtn.style.display = 'block';
+    } else {
+      userNameElem.textContent = '';
+      userPicElem.style.backgroundImage = '';
+      
+      signOutBtn.style.display = 'none';
+      signInBtn.style.display = 'block';
+    } */
+  }
 
   return (
     <>
-      <header>Netflix!</header>
+      {/* <header>Netflix!</header> */}
+      <Header userDate={userDate} />
       <RegistContainer />
       <footer></footer>
     </>
@@ -45,8 +98,8 @@ function RegistContainer() {
           <button type="button" onClick={() => nativeSignInMethod(userCredential, setError)} >Sign in</button>
         </fieldset>
 
-        <button>Login with Google</button>
-        <button>Login with Facebook</button>
+        <button onClick={() => signIn(GoogleAuthProvider)}>Login with Google</button>
+        <button onClick={() => signIn(FacebookAuthProvider)}>Login with Facebook</button>
       </form>
     </Wrapper>
   )
@@ -55,9 +108,11 @@ function RegistContainer() {
 const Wrapper = styled.div`
   
   fieldset {
+    padding-top: 20px;
     display: flex;
     flex-direction: column;
     position: relative;
+    border: none;
 
     &::before {
       content: '${(props) => props.err}';
@@ -70,15 +125,11 @@ const Wrapper = styled.div`
   }
 `
 
-/* content: ${(props) => console.log(props.err)}; */
-      /* content: ${props => props.err ? props.err : props.err}; */
-
 async function nativeSignInMethod(obj, changeErrorState) {
   try {
     await signInWithEmailAndPassword(getAuth(), obj.login, obj.password);
   } catch(err) {
     changeErrorState('Wrong login or password');
-    //console.log('Wrong login or password')
   }
 }
 
@@ -87,38 +138,12 @@ async function signIn(params) {
   await signInWithPopup(getAuth(), provider);
 }
 
-function initFirebaseAuth() {
-  onAuthStateChanged(getAuth(), authStateObserver);
-}
-
 function getUserName() {
   return getAuth().currentUser.displayName;
 }
 
 function getProfilePicUrl() {
   return getAuth().currentUser.photoURL || '/images/profile_placeholder.png';
-}
-
-function authStateObserver(user) {
-  console.log('observe')
-  /* let signInBtn     = document.getElementById('sign-in');
-  let signOutBtn    = document.getElementById('sign-out');
-  let userNameElem  = document.getElementById('user-name');
-  let userPicElem   = document.getElementById('user-pic');
-
-  if (user) {
-    userNameElem.textContent = getUserName();
-    userPicElem.style.backgroundImage = `url(${getProfilePicUrl()})`
-
-    signInBtn.style.display = 'none';
-    signOutBtn.style.display = 'block';
-  } else {
-    userNameElem.textContent = '';
-    userPicElem.style.backgroundImage = '';
-    
-    signOutBtn.style.display = 'none';
-    signInBtn.style.display = 'block';
-  } */
 }
 
 function signOutUser() {
